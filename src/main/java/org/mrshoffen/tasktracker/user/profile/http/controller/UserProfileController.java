6457cd795c1c03e283e613a87c278f00b46a6ae7
andrey_vasiliev <mrshoffen@gmail.com>
2025-04-24 22:25:07 +0300

    feat: add endpoint for validating existing user

--- src/main/java/org/mrshoffen/tasktracker/user/profile/http/controller/UserProfileController.java
+++ src/main/java/org/mrshoffen/tasktracker/user/profile/http/controller/UserProfileController.java
@@ -1,12 +1,14 @@
 package org.mrshoffen.tasktracker.user.profile.http.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.mrshoffen.tasktracker.commons.kafka.event.UserCreatedEvent;
 import org.mrshoffen.tasktracker.user.profile.model.dto.UserCreateDto;
 import org.mrshoffen.tasktracker.user.profile.model.dto.UserResponseDto;
 import org.mrshoffen.tasktracker.user.profile.service.UserService;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,9 +23,17 @@ public class UserProfileController {
 
     private final UserService userService;
 
+    private final Environment env;
+
     @GetMapping("/status")
     String check(){
-        return "OK";
+        return "OK - " + env.getProperty("local.server.port");
+    }
+
+    @GetMapping("/me")
+    ResponseEntity<UserResponseDto> me(HttpServletRequest request){
+
+        return null;
     }
 
     @PostMapping
--- src/main/java/org/mrshoffen/tasktracker/user/profile/http/controller/UserValidationController.java
+++ src/main/java/org/mrshoffen/tasktracker/user/profile/http/controller/UserValidationController.java
@@ -3,6 +3,7 @@ package org.mrshoffen.tasktracker.user.profile.http.controller;
 import lombok.RequiredArgsConstructor;
 import org.mrshoffen.tasktracker.user.profile.model.dto.UserResponseDto;
 import org.mrshoffen.tasktracker.user.profile.service.UserService;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,9 @@ import java.util.UUID;
 public class UserValidationController {
 
     private final UserService userService;
+
+    private final Environment env;
+
 
     @GetMapping
     ResponseEntity<UUID> getUserId(@RequestParam("email") String email,
