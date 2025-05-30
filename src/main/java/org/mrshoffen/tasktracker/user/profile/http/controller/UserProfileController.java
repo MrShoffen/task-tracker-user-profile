6c51f0b1a2d8ef531bdbe25e3d24f075f6ca197c
andrey_vasiliev <mrshoffen@gmail.com>
2025-05-30 18:25:37 +0300

    chore: update dependency platform

--- src/main/java/org/mrshoffen/tasktracker/user/profile/http/controller/UserProfileController.java
+++ src/main/java/org/mrshoffen/tasktracker/user/profile/http/controller/UserProfileController.java
@@ -3,8 +3,10 @@ package org.mrshoffen.tasktracker.user.profile.http.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.mrshoffen.tasktracker.commons.web.dto.UserResponseDto;
+import org.mrshoffen.tasktracker.user.profile.exception.UserNotFoundException;
 import org.mrshoffen.tasktracker.user.profile.model.dto.AvatarEditDto;
 import org.mrshoffen.tasktracker.user.profile.model.dto.ProfileEditDto;
+import org.mrshoffen.tasktracker.user.profile.model.entity.User;
 import org.mrshoffen.tasktracker.user.profile.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -45,6 +48,13 @@ public class UserProfileController {
                                                  @RequestBody AvatarEditDto editDto) {
         return ResponseEntity.ok(
                 userService.updateUserAvatar(userId, editDto.avatarUrl())
+        );
+    }
+
+    @GetMapping("/info")
+    ResponseEntity<UserResponseDto> userInformation(@RequestParam("id") UUID id) {
+        return ResponseEntity.ok(
+                userService.getUserById(id)
         );
     }
 
